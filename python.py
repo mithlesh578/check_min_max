8af5893cbb1aa99762c6aa3a4e5cb4108baa0ecf
mithlesh kumar <mithleshverma9153@gmail.com>
2024-11-26 17:41:45 +0530

    add new line code

--- python.py
+++ python.py
@@ -4,14 +4,14 @@ y = int(input("Enter second number: "))
 z = int(input("Enter third number: "))
 a = int(input("Enter 1 to check max and enter 2 to check min: "))
 if a==1:
-    def max():
+    def max_value():
         m = max(x,y,z)
         print("maximum is :",m)
-    max()
+    max_value()
 elif a==2:
-    def min():
+    def minimum_value():
         n = min(x,y,z)
         print("minimum is :",n)
-    min()
+    minimum_value()
 else:
     print("Invalid Input!")
