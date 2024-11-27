1188714565229aa28ca06c9e8cc855cb11ce0c03
mithlesh kumar <mithleshverma9153@gmail.com>
2024-11-27 08:55:34 +0530

    add comment

---
+++ converter.py
@@ -0,0 +1,5 @@
+x = int(input("Enter dollar: "))
+def converter():
+    #1 USD = 84.37 INR
+    print(f"{x} USD = {x*84.37} INR")
+converter()
---
+++ turtle_design.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle.Pen()
+for i in range(8):
+    t.circle(100)
+    t.color("red")
+    t.circle(90)
+    t.color("black")
+    t.circle(80)
+    t.color("green")
+    t.circle(70)
+    t.color("pink")
+    t.right(45)
+t.hideturtle()
